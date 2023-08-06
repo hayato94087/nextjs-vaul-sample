b5a1bc94152351c35c0945d45a0da88c504cc4ba
hayato94087 <hayato94087@gmail.com>
2023-08-07 06:32:20 +0900

    デザインありのDrawerを実装

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,19 @@
+import DrawerNestedDrawers from "@/components/drawer-nested-drawers";
+import DrawerNonDismissible from "@/components/drawer-non-dismissible";
+import DrawerScrollableWithInputs from "@/components/drawer-scrollable-with-inputs";
 import DrawerWithNoStyle from "@/components/drawer-with-no-style";
+import DrawerWithScaledBackground from "@/components/drawer-with-scaled-background";
+import DrawerWithoutScaledBackground from "@/components/drawer-without-scaled-background";
 
 export default function Home() {
   return (
-    <main className="text-lg">
+    <main className="flex flex-col items-start">
       <DrawerWithNoStyle />
+      <DrawerWithScaledBackground />
+      <DrawerWithoutScaledBackground />
+      <DrawerScrollableWithInputs />
+      <DrawerNestedDrawers />
+      <DrawerNonDismissible /> 
     </main>
   )
 }
--- src/components/drawer-with-no-style.tsx
+++ src/components/drawer-with-no-style.tsx
@@ -1,14 +1,16 @@
-"use client"
+"use client";
 
-import {FC} from 'react'
-import { Drawer } from 'vaul';
+import { FC } from "react";
+import { Drawer } from "vaul";
 
 interface DrawerWithNoStyleProps {}
 
 const DrawerWithNoStyle: FC<DrawerWithNoStyleProps> = ({}) => {
   return (
     <Drawer.Root>
-      <Drawer.Trigger>Open</Drawer.Trigger>
+      <Drawer.Trigger className="bg-slate-800 text-white py-1 px-3 rounded-md m-2">
+          スタイル無しの Drawer
+      </Drawer.Trigger>
       <Drawer.Portal>
         <Drawer.Content>
           <p>Content</p>
